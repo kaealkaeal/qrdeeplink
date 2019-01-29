b0a9e96a95d4948bae572c84995d794702a46d6f
Amonrat Loylertlar <kaealkaeal159@hotmail.com>
2019-01-30 02:50:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,29 @@
 <html>
 <head>
 	<title>qrdeeplink</title>
+	<script src="https://d.line-scdn.net/liff/1.0/sdk.js" /></script>
 </head>
 <body>
 <a href="index.php?var=111">go</a>
+<script>
+liff.init(
+  data => {
+    // Now you can call LIFF API
+    const userId = data.context.userId;
+  }
+);
+</script>
+<script>
+	liff.getProfile()
+.then(profile => {
+  const userid = profile.userId
+})
+.catch((err) => {
+  console.log('error', err);
+});
+</script>
+<script type="text/javascript">
+	window.location="https://qrdeeplink.herokuapp.com?userId="+userid;
+</script>
 </body>
 </html>
