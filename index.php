999bdc93d40e7b245a6c6f6737cae307c2ad446e
Amonrat Loylertlar <kaealkaeal159@hotmail.com>
2019-01-30 03:34:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<script src="https://d.line-scdn.net/liff/1.0/sdk.js" /></script>
 </head>
 <body>
-<meta http-equiv="refresh" content="0; URL='class://scan?userId='+userid" />
+<meta http-equiv="refresh" content="2; URL='class://scan?userId='+userid" />
 <script type="text/javascript">
 	window.location="class://scan?userId="+userid;
 </script>
